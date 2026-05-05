b5e2af836a3f686348ef4d411142369e4c939bb1
Marek Dworaczyk <marek@dworaczyk.eu>
2026-05-05 12:52:32 +0200

    fix: ignore id when saving csv file

--- browser_session.py
+++ browser_session.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 from extract_data import scrap
 import pandas as pd
 
@@ -8,18 +11,27 @@ options = Options()
 options.debugger_address = "127.0.0.1:9222"
 
 driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 10)
 
 tabs = driver.window_handles
-
 #otwarcie bazy danych (pliku csv)
 
-df = pd.read_csv("database.csv") # headers: ID, PRICE, SIZE, ROOMS, DATE, OWNER, LOCATION
+df = pd.read_csv("database.csv") #headers: ID, PRICE, SIZE, ROOMS, DATE, OWNER, LOCATION
 
 #gather rows of data
 data_rows = []
 
 for tab in tabs:
     driver.switch_to.window(tab)
+    #the site needs to load up, thats why we need to scroll down and wait a second, gpt
+
+    #we choose which element we will scroll to (the map)
+    element = driver.find_element(By.CLASS_NAME, "css-nkt06p.e1d73xdh0")
+    driver.execute_script("arguments[0].scrollIntoView();", element)
+    #wait until javascript finishes, gpt
+    #find another way to wait
+    time.sleep(5)
+
     soup = bs(driver.page_source, "lxml")
 
     data = scrap(soup) #otrzymane dane to id,cena,metraz,pokoje,data,wlasciciel,lat,lon
@@ -36,5 +48,8 @@ for tab in tabs:
 new_df = pd.DataFrame(data_rows)
 df = pd.concat([df, new_df], ignore_index=True)
 df = df.drop_duplicates(subset="ID", keep="first")
-print(df)
+
+#save to the file
+df.to_csv("database.csv", index=False)
+
 
